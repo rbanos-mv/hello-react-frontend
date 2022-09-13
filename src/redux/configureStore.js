937342e585a998557154b4b00852503d2477bc6d
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-09-13 11:47:53 -0500

    Configure store

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,7 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
-import Reducer from '.';
+import Reducer from './greetings/greetings';
 
 const middleware = [thunk];
 if (process.env.NODE_ENV !== 'production') {
@@ -11,7 +11,7 @@ if (process.env.NODE_ENV !== 'production') {
 
 const store = configureStore({
   reducer: {
-    xxx: Reducer,
+    greeting: Reducer,
   },
   middleware: [thunk, logger],
 });
