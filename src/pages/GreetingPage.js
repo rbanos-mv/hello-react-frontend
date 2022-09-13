7701fceb8418b1d6a0c11b3000d089408032b928
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-09-13 12:07:00 -0500

    Add instruction

--- src/pages/GreetingPage.js
+++ src/pages/GreetingPage.js
@@ -4,6 +4,9 @@ const GreetingPage = () => {
   const greeting = useSelector((state) => state.greeting, shallowEqual);
   return (
     <div className="container">
+      <h3>Reload page to receive a random greeting</h3>
+      <br />
+      <br />
       <h1>{greeting}</h1>
     </div>
   );
