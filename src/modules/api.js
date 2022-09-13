f609eec4c196334fdd4a293b4e8a34ceaead1bcf
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-09-13 11:40:52 -0500

    Create module to communicate with api

--- src/modules/api.js
+++ src/modules/api.js
@@ -1,1 +1,8 @@
-// import API_URL from './environment';
+const apiGetGreeting = () => fetch('http://localhost:3001/greetings')
+  .then((response) => response.text())
+  .then((text) => JSON.parse(text).message)
+  .catch((error) => {
+    throw error;
+  });
+
+export default apiGetGreeting;
